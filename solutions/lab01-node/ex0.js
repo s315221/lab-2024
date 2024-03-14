f8f619590c92dbf4275bc75b04e79723d2b888fe
s315221 <162451446+s315221@users.noreply.github.com>
2024-03-15 00:14:09 +0100

    add ex0 solution

---
+++ solutions/lab01-node/ex0.js
@@ -0,0 +1,15 @@
+function printShortenedStrings(stringArray = []) {
+    stringArray.forEach(
+        str => {
+            let ret = ""; //store shortened string
+            const len = str.length; // get number of characters in string
+            if (len >= 2) { //if more than 2 characters
+                ret = str[0] + str[1] + str[len - 2] + str[len - 1]; // result is the first two characters and last two characters cocatenated
+            }
+            console.log(`"${str}" : "${ret}"`);
+        }
+    )
+}
+
+const stringArray = ["spring", "it", "cat"];
+printShortenedStrings(stringArray);
