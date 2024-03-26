a432effc251b536c90202aacb02097fd1ae8be12
s315221 <162451446+s315221@users.noreply.github.com>
2024-03-26 10:40:09 +0100

    update lab01

--- mySolutions/lab01-node/FilmLibrary.mjs
+++ mySolutions/lab01-node/FilmLibrary.mjs
@@ -14,7 +14,7 @@ function Film(id, title, isFavorite = false, date, rating, userId = 1) {
     this.rating = rating;
     this.userId = userId;
 
-    //`Id: ${film.id}, Title: ${film.title}, Favorite: ${film.isFavorite}, Watch date: ${film.date}, Score: ${film.rating}, User: ${film.userId}`;
+    this.toString = () => `Id: ${this.id}, Title: ${this.title}, Favorite: ${this.isFavorite}, Watch date: ${this.date}, Score: ${this.rating}, User: ${this.userId}`;
 
 }
 
@@ -43,29 +43,45 @@ function FilmLibrary() {
 }
 
 
-let filmLibrary = new FilmLibrary();
-filmLibrary.addNewFilm(new Film(1, "Pulp Fiction", true, "March 10, 2024", 5, 1));
-filmLibrary.addNewFilm(new Film(2, "21 Grams", true, "March 17, 2024", 4, 1));
-filmLibrary.addNewFilm(new Film(3, "Star Wars", false, null, 0, 1));
-filmLibrary.addNewFilm(new Film(4, "Matrix", false, null, 0, 1));
-filmLibrary.addNewFilm(new Film(5, "Shrek", false, "March 21, 2024", 3, 1));
+function main() {
 
-console.log(filmLibrary);
-console.log("\n\n\n\nSorted by date:\n", filmLibrary.sortByDate());
+    // Creating some film entries
+    const pulpFiction = new Film(1, "Pulp Fiction", true, "2024-03-10", 5);
+    const grams21 = new Film(2, "21 Grams", true, "2024-03-17", 4);
+    const starWars = new Film(3, "Star Wars", false);
+    const matrix = new Film(4, "Matrix");
+    const shrek = new Film(5, "Shrek", false, "2024-03-21", 3);
 
-console.log("\n\n\n\nDelete film id 3");
-filmLibrary.deleteFilm(3);
-console.log(filmLibrary);
-filmLibrary.addNewFilm(new Film(3, "Star Wars", false, null, 0, 1));
+    let filmLibrary = new FilmLibrary();
+    filmLibrary.addNewFilm(pulpFiction);
+    filmLibrary.addNewFilm(grams21);
+    filmLibrary.addNewFilm(starWars);
+    filmLibrary.addNewFilm(matrix);
+    filmLibrary.addNewFilm(shrek);
 
-console.log("\n\n\n\nresetWatchFilms:\n");
-filmLibrary.resetWatchedFilms();
-console.log(filmLibrary);
-filmLibrary = new FilmLibrary();
-filmLibrary.addNewFilm(new Film(1, "Pulp Fiction", true, "March 10, 2024", 5, 1));
-filmLibrary.addNewFilm(new Film(2, "21 Grams", true, "March 17, 2024", 4, 1));
-filmLibrary.addNewFilm(new Film(3, "Star Wars", false, null, 0, 1));
-filmLibrary.addNewFilm(new Film(4, "Matrix", false, null, 0, 1));
-filmLibrary.addNewFilm(new Film(5, "Shrek", false, "March 21, 2024", 3, 1));
+    //list films
+    console.log("*****List of films*****")
+    console.log(filmLibrary.films.map(film => film.toString()));
 
-console.log("\n\n\n\nget rated:\n", filmLibrary.getRated());
+
+    console.log("\n\n\n\nSorted by date:\n", filmLibrary.sortByDate().map(film => film.toString()));
+
+    console.log("\n\n\n\nDelete film id 3");
+    filmLibrary.deleteFilm(3);
+    console.log(filmLibrary.films.map(film => film.toString()));
+    //filmLibrary.addNewFilm(new Film(3, "Star Wars", false, null, 0, 1));
+
+    console.log("\n\n\n\nresetWatchFilms:\n");
+    filmLibrary.resetWatchedFilms();
+    console.log(filmLibrary.films.map(film => film.toString()));
+    filmLibrary = new FilmLibrary();
+    filmLibrary.addNewFilm(new Film(1, "Pulp Fiction", true, "March 10, 2024", 5, 1));
+    filmLibrary.addNewFilm(new Film(2, "21 Grams", true, "March 17, 2024", 4, 1));
+    filmLibrary.addNewFilm(new Film(3, "Star Wars", false, null, 0, 1));
+    filmLibrary.addNewFilm(new Film(4, "Matrix", false, null, 0, 1));
+    filmLibrary.addNewFilm(new Film(5, "Shrek", false, "March 21, 2024", 3, 1));
+
+    console.log("\n\n\n\nget rated:\n", filmLibrary.getRated().map(film => film.toString()));
+}
+
+main();
