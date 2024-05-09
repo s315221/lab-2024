1775b43413a59a854aaff075f3af24c021a59376
s315221 <162451446+s315221@users.noreply.github.com>
2024-05-09 13:56:36 +0200

    add filter button update

--- lab05-js-browser/mySolution/public/FilmLibrary.mjs
+++ lab05-js-browser/mySolution/public/FilmLibrary.mjs
@@ -1,9 +1,5 @@
 "use strict";
 
-/**
- * 1.  Create a Film Library
- */
-
 function Film(id, title, isFavorite = false, date, rating, userId = 1) {
     this.id = id;
     this.title = title;
@@ -21,71 +17,35 @@ function FilmLibrary() {
 
     this.addNewFilm = (film) => this.films.push(film);
 
-    // 2
     this.sortByDate = () => [...this.films].sort((a, b) =>
         !a.date ? 1 : // if first date null, then it is larger so +1
             !b.date ? -1 : // else if second date is null, then it is larger so -1
                 dayjs(a.date).diff(dayjs(b.date))  // else return dayjs difference
     );
 
-    // 2
     this.deleteFilm = (id) => this.films = this.films.filter(film => film.id != id);
 
-    // 2
     this.resetWatchedFilms = () => this.films.forEach(film => film.date = null);
 
-    // 2
     this.getRated = (rating) => this.films.filter(film => film.rating).sort((a, b) => b.score - a.score);
 
+    /*Exercise 2*/
+    this.getFavorites = () => this.films.filter(film => film.isFavorite);
 
+    this.getBestRated = () => this.films.filter(film => film.rating >= 5);
+
+    /**
+     * @param {string} date
+     */
+    this.getSeenAfter = (date) => {
+        console.log(date, dayjs())
+        return this.films.filter(film => film.date && dayjs(film.date).isSame(dayjs(date)));
+    }
+
+    this.getUnSeen = () => this.films.filter(film => !film.date)
 }
-
-
-function mainConsole() {
-
-    // Creating some film entries
-    const pulpFiction = new Film(1, "Pulp Fiction", true, "2024-03-10", 5);
-    const grams21 = new Film(2, "21 Grams", true, "2024-03-17", 4);
-    const starWars = new Film(3, "Star Wars", false);
-    const matrix = new Film(4, "Matrix");
-    const shrek = new Film(5, "Shrek", false, "2024-03-21", 3);
-
-    let filmLibrary = new FilmLibrary();
-    filmLibrary.addNewFilm(pulpFiction);
-    filmLibrary.addNewFilm(grams21);
-    filmLibrary.addNewFilm(starWars);
-    filmLibrary.addNewFilm(matrix);
-    filmLibrary.addNewFilm(shrek);
-
-    //list films
-    console.log("*****List of films*****")
-    console.log(filmLibrary.films.map(film => film.toString()));
-
-
-    console.log("\n\n\n\nSorted by date:\n", filmLibrary.sortByDate().map(film => film.toString()));
-
-    console.log("\n\n\n\nDelete film id 3");
-    filmLibrary.deleteFilm(3);
-    console.log(filmLibrary.films.map(film => film.toString()));
-    //filmLibrary.addNewFilm(new Film(3, "Star Wars", false, null, 0, 1));
-
-    console.log("\n\n\n\nresetWatchFilms:\n");
-    filmLibrary.resetWatchedFilms();
-    console.log(filmLibrary.films.map(film => film.toString()));
-    filmLibrary = new FilmLibrary();
-    filmLibrary.addNewFilm(new Film(1, "Pulp Fiction", true, "March 10, 2024", 5, 1));
-    filmLibrary.addNewFilm(new Film(2, "21 Grams", true, "March 17, 2024", 4, 1));
-    filmLibrary.addNewFilm(new Film(3, "Star Wars", false, null, 0, 1));
-    filmLibrary.addNewFilm(new Film(4, "Matrix", false, null, 0, 1));
-    filmLibrary.addNewFilm(new Film(5, "Shrek", false, "March 21, 2024", 3, 1));
-
-    console.log("\n\n\n\nget rated:\n", filmLibrary.getRated().map(film => film.toString()));
-}
-
 
 /**Lab 05 **/
-
-
 
 /*Exercise 1*/
 
@@ -94,7 +54,7 @@ function mainConsole() {
  * @returns {string} film as html td element
  */
 function filmToTableRow(film) {
-
+    console.log('populating ', film)
     const ratingToIcons = function (rating) {
         return [1, 2, 3, 4, 5]
             .map(i => `<i class="bi bi-star${rating >= i ? "-fill" : ''}"></i>`)
@@ -124,26 +84,31 @@ function filmToTableRow(film) {
 
 
 /**
- * @param {FilmLibrary} filmLibrary 
+ * @param {Film[]} filmArray 
  * @returns void
  * inserts films in filmLibrary as a table rows in film table
  */
-function pagePopulateFilmTable(filmLibrary) {
+function pagePopulateFilmTable(filmArray) {
 
     const filmListElement = document.getElementById("film-list");
-    filmListElement.innerHTML = filmLibrary.films.map(
+    filmListElement.innerHTML = filmArray.map(
         film => filmToTableRow(film)
     ).join('\n');
+}
+
+function pageSetActiveFilter() {
+
 }
 
 
 function main() {
     // Creating some film entries
-    const pulpFiction = new Film(1, "Pulp Fiction", true, "2024-03-10", 5);
-    const grams21 = new Film(2, "21 Grams", true, "2024-03-17", 4);
+    const pulpFiction = new Film(1, "Pulp Fiction", true, "2024-04-10", 5);
+    const grams21 = new Film(2, "21 Grams", true, "2024-04-17", 4);
     const starWars = new Film(3, "Star Wars", false);
-    const matrix = new Film(4, "Matrix");
-    const shrek = new Film(5, "Shrek", false, "2024-03-21", 3);
+    const matrix = new Film(4, "Matrix", true);
+    const shrek = new Film(5, "Shrek", false, "2024-04-21", 3);
+    const shrek2 = new Film(5, "Shrek", false, "2024-04-01", 3);
 
     let filmLibrary = new FilmLibrary();
     filmLibrary.addNewFilm(pulpFiction);
@@ -151,10 +116,58 @@ function main() {
     filmLibrary.addNewFilm(starWars);
     filmLibrary.addNewFilm(matrix);
     filmLibrary.addNewFilm(shrek);
+    filmLibrary.addNewFilm(shrek2);
 
-    pagePopulateFilmTable(filmLibrary);
+    /*Exercise 1*/
+    pagePopulateFilmTable(filmLibrary.films);
+
+    /*Exercise 2*/
+
+    /**
+    * @param {Event} event
+    * @param {FilmLibrary} filmLibrary 
+    */
+
+    let previousSelectedFilterButton = document.getElementById("filter-all");
+
+    function filterHandler(event, filmLibrary) {
+        const filterButton = event.target;
+        const filterId = filterButton.id;
+        switch (filterId) {
+            case "filter-all":
+                pagePopulateFilmTable(filmLibrary.films);
+                break;
+            case "filter-favorites":
+                pagePopulateFilmTable(filmLibrary.getFavorites());
+                break;
+            case "filter-bestRated":
+                pagePopulateFilmTable(filmLibrary.getBestRated());
+                break;
+            case "filter-seenLastMonth":
+                pagePopulateFilmTable(filmLibrary.getSeenAfter(dayjs().subtract(1, 'month').startOf('month')));
+                break;
+            case "filter-unseen":
+                pagePopulateFilmTable(filmLibrary.getUnSeen());
+                break;
+            default:
+                pagePopulateFilmTable([]);
+        }
+
+
+        previousSelectedFilterButton.className = previousSelectedFilterButton.className.replace(" active", "");
+        let currentSelectedFilterButton = document.getElementById(filterId);
+        currentSelectedFilterButton.className = currentSelectedFilterButton.className.concat(" active");
+        previousSelectedFilterButton = currentSelectedFilterButton;
+
+    }
+
+    const filterButtonElements = document.getElementsByTagName("filter-button");
+    for (const filterButtonElement of filterButtonElements) {
+        filterButtonElement.addEventListener('click', (e) => filterHandler(e, filmLibrary));
+    }
 
 
 }
+
 
 main();
